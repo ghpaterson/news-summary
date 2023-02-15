3164818bee551f0e785f1a404f1062e6814e1b87
Graeme Paterson <ghpaterson@gmail.com>
2023-02-15 15:30:41 +0000

    NewsView class created and passing tests in jest

--- newsView.js
+++ newsView.js
@@ -1,3 +1,19 @@
-class NewsView {}
+class NewsView {
+  constructor(model) {
+    this.model = model;
+    this.mainContainerEl = document.querySelector("#main-container");
+  }
+
+  displayNews() {
+    const news = this.model.getNews();
+
+    news.forEach((item) => {
+      const newNews = document.createElement("div");
+      newNews.className = "news-item";
+      newNews.textContent = item;
+      this.mainContainerEl.append(newNews);
+    });
+  }
+}
 
 module.exports = NewsView;
