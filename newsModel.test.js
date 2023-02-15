0d019da8cd5761a706c6188ee6d6f506e24cd082
Graeme Paterson <ghpaterson@gmail.com>
2023-02-15 14:40:20 +0000

    newsModel tests written

---
+++ newsModel.test.js
@@ -0,0 +1,23 @@
+describe("News model", () => {
+  it("creates and empty array", () => {
+    const model = new NewsModel();
+    expect(model.getNews()).toEqual([]);
+  });
+
+  xit("returns an array of news articles", () => {
+    const model = new NewsModel();
+    const itemOne = "Dog learns to pay taxes";
+    const itemTwo = "Are we all doomed?";
+    model.addNews(itemOne);
+    model.addNews(itemTwo);
+    expect(model.getNews()).toEqual([itemOne, itemTwo]);
+  });
+
+  xit("resets the array of news articles", () => {
+    const model = new NewsModel();
+    const itemOne = "Are trees smarter than us?";
+    model.addNews(itemOne);
+    model.reset();
+    expect(model.getNews()).toEqual([]);
+  });
+});
