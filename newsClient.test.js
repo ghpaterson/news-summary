a52793ae72e88036ee9446fc83ab92b170df5dde
Graeme Paterson <ghpaterson@gmail.com>
2023-02-16 14:53:03 +0000

    newsClient test created

---
+++ newsClient.test.js
@@ -0,0 +1,27 @@
+const NewsClient = require("./newsClient");
+
+// This makes `fetch` available to our test
+// (it is not by default, as normally `fetch` is only
+// available within the browser)
+
+require("jest-fetch-mock").enableMocks();
+
+describe("NewsClient class", () => {
+  it("calls the fetch and loads the lastest news articles", (done) => {
+    const client = new NewsClient();
+
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        status: "ok",
+        currentPage: 1,
+      })
+    );
+
+    client.loadHeadlines((returnedDataFromApi) => {
+      expect(returnedDataFromApi.status).toBe("ok");
+      expect(returnedDataFromApi.currentPage).toBe(1);
+
+      done();
+    });
+  });
+});
